07a6f4b1d7fab60e51111b14f226290c0212e08f
Abhishek-Kharpal <abhikharpal@gmail.com>
2023-07-05 00:23:03 +0530

    feat: add form to send message

--- src/components/chatBot/index.tsx
+++ src/components/chatBot/index.tsx
@@ -1,14 +1,63 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { ActivateContext } from '../../contexts/activate';
+import { ChatContext } from '@src/contexts/chat';
 import Activate from './activate';
 
 const ChatBot = () => {
   const { activate } = useContext(ActivateContext);
+  const { chats, setChat } = useContext(ChatContext);
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    setChat([...chats, { fromAuthor: true, message }]);
+    setMessage('');
+  };
+
   return (
-    <div className={activate ? 'hide-chat-bot' : 'show-chat-bot'}>
+    <div className={activate ? 'show-chat-bot' : 'hide-chat-bot'}>
       <div>
-        <h1>ChatBot</h1>
-        <Activate />
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            gap: '10px',
+          }}
+        >
+          <h1>ChatBot</h1>
+          <Activate />
+        </div>
+        {activate && (
+          <>
+            {chats.map((chat, index) => {
+              return (
+                <div key={index}>
+                  {chat.fromAuthor ? (
+                    <div className="from-author">
+                      <p>{chat.message}</p>
+                    </div>
+                  ) : (
+                    <div className="from-GPT">
+                      <p>{chat.message}</p>
+                    </div>
+                  )}
+                </div>
+              );
+            })}
+            <form onSubmit={handleSubmit}>
+              <input
+                type="text"
+                value={message}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                  setMessage(e.target.value);
+                }}
+                className="message-form"
+              />
+              <button type="submit">Send</button>
+            </form>
+          </>
+        )}
       </div>
     </div>
   );
--- src/contexts/chat.tsx
+++ src/contexts/chat.tsx
@@ -6,13 +6,13 @@ interface Chat {
 }
 
 export const ChatContext = createContext({
-  chat: [] as Chat[],
+  chats: [] as Chat[],
   setChat: (chat: Chat[]) => {},
 });
 
 // TODO: will be getting chat from local storage
 
 export const ChatProvider = ({ children }: any) => {
-  const [chat, setChat] = useState([] as Chat[]);
-  return <ChatContext.Provider value={{ chat, setChat }}>{children}</ChatContext.Provider>;
+  const [chats, setChat] = useState([] as Chat[]);
+  return <ChatContext.Provider value={{ chats, setChat }}>{children}</ChatContext.Provider>;
 };
