9f8fd735fd9601dd6c78e53b4d727ba802e08dec
Abhishek-Kharpal <abhikharpal@gmail.com>
2023-07-05 00:47:19 +0530

    feat: add api call to get response of message

--- src/components/chatBot/index.tsx
+++ src/components/chatBot/index.tsx
@@ -2,15 +2,30 @@ import { useContext, useState } from 'react';
 import { ActivateContext } from '../../contexts/activate';
 import { ChatContext } from '@src/contexts/chat';
 import Activate from './activate';
+import { API_URL } from './constants';
 
 const ChatBot = () => {
   const { activate } = useContext(ActivateContext);
   const { chats, setChat } = useContext(ChatContext);
   const [message, setMessage] = useState('');
+  const [loader, setLoader] = useState(false);
 
-  const handleSubmit = (e: React.FormEvent) => {
+  // TODO: set error handling
+  const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    setChat([...chats, { fromAuthor: true, message }]);
+    setLoader(true);
+    const data = { message };
+    const response = await fetch(API_URL, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(data),
+    });
+    const newChat = await response.json();
+    // newChat.message is response from GPT
+    setTimeout(() => {
+      setChat([...chats, { fromAuthor: true, message }, { fromAuthor: false, message: newChat.message }]);
+      setLoader(false);
+    }, 1000);
     setMessage('');
   };
 
@@ -30,32 +45,52 @@ const ChatBot = () => {
         </div>
         {activate && (
           <>
-            {chats.map((chat, index) => {
-              return (
-                <div key={index}>
-                  {chat.fromAuthor ? (
-                    <div className="from-author">
-                      <p>{chat.message}</p>
-                    </div>
-                  ) : (
-                    <div className="from-GPT">
-                      <p>{chat.message}</p>
-                    </div>
-                  )}
-                </div>
-              );
-            })}
-            <form onSubmit={handleSubmit}>
-              <input
-                type="text"
-                value={message}
-                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                  setMessage(e.target.value);
-                }}
-                className="message-form"
-              />
-              <button type="submit">Send</button>
-            </form>
+            <div
+              style={{
+                overflowY: 'scroll',
+                height: '300px',
+              }}
+            >
+              {chats.map((chat, index) => {
+                return (
+                  <div key={index}>
+                    {chat.fromAuthor ? (
+                      <div className="from-author">
+                        <p>{chat.message}</p>
+                      </div>
+                    ) : (
+                      <div className="from-GPT">
+                        <p>{chat.message}</p>
+                      </div>
+                    )}
+                  </div>
+                );
+              })}
+              {loader && (
+                <>
+                  <div className="from-author">
+                    <p>{message}</p>
+                  </div>
+                  <div className="from-GPT">
+                    <p>Loading...</p>
+                  </div>
+                </>
+              )}
+            </div>
+            <div className="form-container">
+              <form onSubmit={handleSubmit}>
+                <input
+                  type="text"
+                  value={message}
+                  onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                    setMessage(e.target.value);
+                  }}
+                  className="input-message"
+                  placeholder="Type your message here..."
+                />
+                <button type="submit">Send</button>
+              </form>
+            </div>
           </>
         )}
       </div>
